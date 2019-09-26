efe000c512e4a1d1b36f88535b35ae3b998532cf
Trevor Fountain <trevor@texasexpat.net>
2019-09-26 17:36:20 +0200

    First publish

---
+++ src/index.tsx
@@ -0,0 +1,65 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import ImageGallery from "react-image-gallery";
+import shuffle from "lodash-es/shuffle";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+
+const title = require("./img/Titlemark.png");
+const titleVertical = require("./img/Titlemark-vertical.png");
+
+const mp4 = require("./video/promo-hd.mp4");
+
+import "./index.less";
+
+const root = document.createElement("div");
+document.body.appendChild(root);
+
+let screenshots: any[] = [];
+
+for (var i = 1; i <= 15; i++) {
+    const thumb = require(`./thumbs/screenshot${i}.jpg`);
+    const image = require(`./gallery/screenshot${i}.png`);
+    screenshots.push({
+        original: image,
+        thumbnail: thumb,
+    });
+}
+
+screenshots = shuffle(screenshots);
+
+ReactDOM.render((
+    <div>
+        <div className="background">
+        </div>
+        <div className="overlay" />
+        <div className="hero">
+            <div className="background-video scanlines">
+                <video playsInline autoPlay muted loop>
+                    <source src={mp4} type="video/mp4" />
+                </video>
+            </div>
+        </div>
+        <div className="content">
+            <div className="inner-content">
+                <div className="title">
+                    <img src={title} className="title-horizontal" />
+                    <img src={titleVertical} className="title-vertical" />
+                </div>
+                <div className="section section-blue">
+                    <p><strong>The Zeta Orbital</strong> is a first-person roguelite set onboard an abandoned space station. Descend through the station's decks, scavenging for vital supplies as you uncover clues to why the station is abandoned.</p>
+                    <p>Coming to Steam Early Access December 2019!</p>
+                </div>
+                <div className="section">
+                    <ImageGallery items={screenshots} />
+                </div>
+                <div className="section section-blue">
+                    <p>The look of <strong>The Zeta Orbital</strong> is inspired by classic sci-fi games like System Shock and Marathon, combined with in-world puzzles reminiscent of Myst and the "what happened here" exploration of games like Gone Home or The Station.</p>
+                </div>
+                <div className="section section-green">
+                    <p>Want to keep up with <strong>The Zeta Orbital</strong>'s development? Follow <a href="https://twitter.com/doches">@doches <FontAwesomeIcon icon={faTwitter} /></a> on Twitter.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+), root);
---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackTemplate = require("html-webpack-template");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  devtool: "eval",
+  entry: './src/index.tsx',
+  output: {
+    filename: 'app-[hash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /(node_modules)/,
+        options: {
+          configFile: "tsconfig.json"
+        }
+      }, {
+        test: /\.(le|sa|sc|c)ss$/,
+        exclude: [path.resolve(__dirname, "node_modules")],
+        use: ["style-loader", "css-loader", "less-loader"]
+      }, {
+        test: /\.(jpg|png|svg|eot|woff|woff2|ttf)$/,
+        use: [{
+          loader: "file-loader",
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }]
+      }, {
+        test: /\.html$/,
+        loader: "html-loader"
+      }, {
+        test: /\.(mp4|ogg|webm)$/,
+        use: [{
+          loader: "file-loader",
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'video/'
+          }
+        }]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      ".tsx",
+      ".ts",
+      ".js",
+      ".less"
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: HtmlWebpackTemplate,
+      favicon: "src/img/favicon.png",
+      // googleAnalytics: {
+      //   trackingId: "UA-91832446-1",
+      //   pageViewOnLoad: true,
+      // },
+      meta: [
+        {
+          name: "description",
+          content: ""
+        },
+        {
+          name: "keywords",
+          content: ""
+        }
+      ],
+      lang: "en-US",
+      title: "The Zeta Orbital"
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: "static"
+      }
+    ])
+  ],
+  devServer: {
+    clientLogLevel: "warning",
+    compress: false,
+    contentBase: path.join(__dirname, "src")
+  }
+};
